eb68037cc7057c7b60968245667f1d8d3a988d25
Jake Bertish <bertishprofessional@gmail.com>
2024-12-12 20:49:32 -0600

    Fix import

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-//import Leaderboard from './components/Leaderboard.jsx';
-//<Leaderboard />
+import Leaderboard from './components/leaderboard';
+
 
 function App() {
   return (
     <div>
       <h1>Game Picks Leaderboard</h1>
+      <Leaderboard />
     </div>
   );
 }
