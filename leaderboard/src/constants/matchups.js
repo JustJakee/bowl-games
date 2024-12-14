6af4a7032ecd3d9e6f73dfaf7d53f5d47998f2e2
KOONS21 <jake_koons@yahoo.com>
2024-12-14 16:55:16 -0600

    add order to matchups (BOBBY's ORDER)

--- leaderboard/src/constants/matchups.js
+++ leaderboard/src/constants/matchups.js
@@ -3,241 +3,282 @@ const matchups = [
     game: "Salute to Veterans Bowl",
     team1: "South Alabama",
     team2: "Western Michigan",
-    winner: ""
+    winner: "",
+    date: "0"
   },
   {
     game: "Celebration Bowl",
     team1: "South Carolina State",
     team2: "Jackson State",
-    winner: ""
+    winner: "",
+    date: "1"
   },
   {
     game: "Frisco Bowl",
     team1: "Memphis",
     team2: "West Virginia",
-    winner: ""
+    winner: "",
+    date: "2"
+    
   },
   {
     game: "Boca Raton Bowl",
     team1: "James Madison",
     team2: "Western Kentucky",
-    winner: ""
+    winner: "",
+    date: "3"
   },
   {
     game: "LA Bowl",
     team1: "UNLV",
     team2: "Cal",
-    winner: ""
+    winner: "",
+    date: "4"
   },
   {
     game: "New Orleans Bowl",
     team1: "Sam Houston",
     team2: "Georgia Southern",
-    winner: ""
+    winner: "",
+    date: "5"
   },
   {
     game: "Cure Bowl",
     team1: "Ohio",
     team2: "Jacksonville State",
-    winner: ""
+    winner: "",
+    date: "6"
   },
   {
     game: "Gasparilla Bowl",
     team1: "Florida",
     team2: "Tulane",
-    winner: ""
+    winner: "",
+    date: "7"
   },
   {
     game: " College Football Playoff First Round Game  ",
     team1: "Indiana",
     team2: "Notre Dame",
-    winner: ""
+    winner: "",
+    date: "8"
   },
   {
     game: "College Football Playoff First Round Game ",
     team1: "SMU",
     team2: "Penn State",
-    winner: ""
+    winner: "",
+    date: "9"
   },
   {
     game: " College Football Playoff First Round Game ",
     team1: "Clemson",
     team2: "Texas",
-    winner: ""
+    winner: "",
+    date: "10"
   },
   {
     game: " College Football Playoff First Round Game",
     team1: "Tennessee",
     team2: "Ohio State",
-    winner: ""
+    winner: "",
+    date: "11"
   },
   {
     game: "Myrtle Beach Bowl",
     team1: "Coastal Carolina",
     team2: "UTSA",
-    winner: ""
+    winner: "",
+    date: "12"
   },
   {
     game: "Famous Idaho Potato Bowl",
     team1: "Northern Illinois",
     team2: "Fresno State",
-    winner: ""
+    winner: "",
+    date: "13"
   },
   {
     game: "Hawai'i Bowl",
     team1: "South Florida",
     team2: "San Jose State",
-    winner: ""
+    winner: "",
+    date: "14"
   },
   {
     game: "GameAbove Sports Bowl",
     team1: "Pitt",
     team2: "Toledo",
-    winner: ""
+    winner: "",
+    date: "15"
   },
   {
     game: "Rate Bowl",
     team1: "Rutgers",
     team2: "Kansas State",
-    winner: ""
+    winner: "",
+    date: "16"
   },
   {
     game: "68 Ventures Bowl",
     team1: "Arkansas State",
     team2: "Bowling Green",
-    winner: ""
+    winner: "",
+    date: "17"
   },
   {
     game: "Holiday Bowl",
     team1: "Syracuse",
     team2: "Washington State",
-    winner: ""
+    winner: "",
+    date: "18"
   },
   {
     game: "Birmingham Bowl",
     team1: "Georgia Tech",
     team2: "Vanderbilt",
-    winner: ""
+    winner: "",
+    date: "19"
   },
   {
     game: "Armed Forces Bowl",
     team1: "Navy",
     team2: "Oklahoma",
-    winner: ""
+    winner: "",
+    date: "20"
   },
   {
     game: "Liberty Bowl",
     team1: "Arkansas",
     team2: "Texas Tech",
-    winner: ""
+    winner: "",
+    date: "21"
   },
   {
     game: "Las Vegas Bowl",
     team1: "USC",
     team2: "Texas A&M",
-    winner: ""
+    winner: "",
+    date: "22"
   },
   {
     game: "Fenway Bowl",
     team1: "UConn",
     team2: "North Carolina",
-    winner: ""
+    winner: "",
+    date: "23"
   },
   {
     game: "Pinstripe Bowl",
     team1: "Boston College",
     team2: "Nebraska",
-    winner: ""
+    winner: "",
+    date: "24"
   },
   {
     game: "New Mexico Bowl",
     team1: "TCU",
     team2: "Louisiana",
-    winner: ""
+    winner: "",
+    date: "25"
   },
   {
     game: "Pop-Tarts Bowl",
     team1: "Miami",
     team2: "Iowa State",
-    winner: ""
+    winner: "",
+    date: "26"
   },
   {
     game: "Arizona Bowl",
     team1: "Colorado State",
     team2: "Miami (Ohio)",
-    winner: ""
+    winner: "",
+    date: "27"
   },
   {
     game: "Military Bowl",
     team1: "NC State",
     team2: "East Carolina",
-    winner: ""
+    winner: "",
+    date: "28"
   },
   {
     game: "Alamo Bowl",
     team1: "BYU",
     team2: "Colorado",
-    winner: ""
+    winner: "",
+    date: "29"
   },
   {
     game: "Independence Bowl",
     team1: "Army",
     team2: "Marshall",
-    winner: ""
+    winner: "",
+    date: "30"
   },
   {
     game: "Music City Bowl",
     team1: "Missouri",
     team2: "Iowa",
-    winner: ""
+    winner: "",
+    date: "31"
   },
   {
     game: "ReliaQuest Bowl",
     team1: "Alabama",
     team2: "Michigan",
-    winner: ""
+    winner: "",
+    date: "32"
   },
   {
     game: "Sun Bowl",
     team1: "Louisville",
     team2: "Washington",
-    winner: ""
+    winner: "",
+    date: "33"
   },
   {
     game: "Citrus Bowl",
     team1: "South Carolina",
     team2: "Illinois",
-    winner: ""
+    winner: "",
+    date: "34"
   },
   {
     game: "Texas Bowl",
     team1: "LSU",
     team2: "Baylor",
-    winner: ""
+    winner: "",
+    date: "35"
   },
   {
     game: "Gator Bowl",
     team1: "Ole Miss",
     team2: "Duke",
-    winner: ""
+    winner: "",
+    date: "36"
   },
   {
     game: "First Responder Bowl",
     team1: "North Texas",
     team2: "Texas State",
-    winner: ""
+    winner: "",
+    date: "37"
   },
   {
     game: "Duke's Mayo Bowl",
     team1: "Minnesota",
     team2: "Virginia Tech",
-    winner: ""
+    winner: "",
+    date: "38"
   },
   {
     game: "Bahamas Bowl",
     team1: "Liberty",
     team2: "Buffalo",
-    winner: ""
+    winner: "",
+    date: "39"
   }
 ];
 
